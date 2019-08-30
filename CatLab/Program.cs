56a98e2c9ae0b435aeefe3aaf9822a5dae6de9c5
Samoondeeswari Sheela <SSengamalam@spartaglobal.com>
2019-08-30 10:16:22 +0100

    changes

--- CatLab/Program.cs
+++ CatLab/Program.cs
@@ -12,49 +12,36 @@ namespace CatLab
 
             Method.age5(cat);
             Method.age5(cat);
-           // Console.WriteLine();
+          
             var json = new JavaScriptSerializer().Serialize(cat);
             Console.WriteLine(json);
 
             double reverseName;
-            double value = multiply.integerMultiply(3, 3, 2);
-            Console.WriteLine(value);
+         
+            
 
             double value2 = multiply.integerSum(3, 3, 2, out reverseName);
-            Console.WriteLine(value2);
+            Console.WriteLine(reverseName);
+           Console.WriteLine(value2);
             Console.ReadLine();
-
-
-
         }
-
         class CatClass
 
         {
             public string name = "Tilly";
             public int age = 2;
-
-
         }
         class Method
         {
             public static void age5(CatClass cat1) { cat1.age += 5; }
-
-
         }
     }
     class integer
     {
-        public double integerMultiply(double num1, double num2, double num3)//, out double reversename)
-        {
-
-
-            return (num1 * num2 * num3);
-
-        }
+ 
         public double integerSum(double num1, double num2, double num3, out double reversename)
         {
-            reversename = num1 + num2 + num3;
+            reversename = num1 * num2 * num3;
 
             return (num1 + num2 + num3);
         }
