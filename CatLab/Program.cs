8a6b1b45d223d710b7c2ccb3c656bc2146a92a10
Samoondeeswari Sheela <SSengamalam@spartaglobal.com>
2019-08-30 09:59:18 +0100

    changes

--- CatLab/Program.cs
+++ CatLab/Program.cs
@@ -9,10 +9,10 @@ namespace CatLab
         {
             integer multiply = new integer();
             CatClass cat = new CatClass();
-            
+
             Method.age5(cat);
             Method.age5(cat);
-           Console.WriteLine();
+           // Console.WriteLine();
             var json = new JavaScriptSerializer().Serialize(cat);
             Console.WriteLine(json);
 
@@ -23,74 +23,44 @@ namespace CatLab
             double value2 = multiply.integerSum(3, 3, 2, out reverseName);
             Console.WriteLine(value2);
             Console.ReadLine();
-         
-         
+
+
 
         }
 
         class CatClass
 
-    {
-        public string name = "Tilly";
-        public int age = 2;
-        public string Name
         {
-            get
-            {
-                return name;
-            }
-            set
-            {
-                name = value;
-            }
-        }
-             public int Age
-        {
-            get
-            {
-                return age;
-            }
-            set
-            {
-                age = value;
-            }
+            public string name = "Tilly";
+            public int age = 2;
 
-      
-        }
-        public override string ToString()
-        {
-            return "Name = " + Name + ", Age = " + Age;
-        }
 
-    }
+        }
         class Method
         {
-            public static void age5(CatClass cat1) { cat1.age += 5;}
+            public static void age5(CatClass cat1) { cat1.age += 5; }
 
 
         }
-
-
-        
     }
-        class integer
+    class integer
+    {
+        public double integerMultiply(double num1, double num2, double num3)//, out double reversename)
         {
-            public double integerMultiply(double num1, double num2, double num3)//, out double reversename)
-            {
-                
+
 
             return (num1 * num2 * num3);
 
-            }
-            public double integerSum(double num1, double num2, double num3, out double reversename)
-            {
+        }
+        public double integerSum(double num1, double num2, double num3, out double reversename)
+        {
             reversename = num1 + num2 + num3;
 
             return (num1 + num2 + num3);
-            }
-        
-        
         }
-        }
-    
+
+
+    }
+}
+
 
